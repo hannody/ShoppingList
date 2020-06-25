e2c354083e5ed4e443668ff0cf78a13ceb8244ac
hannody <aljaboori@gmail.com>
2020-06-25 18:30:38 +0800

    [Update] linking repository to the factory for MVVM.

--- src/main/java/com/abunayla/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ src/main/java/com/abunayla/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.abunayla.shoppinglist.R
+import com.abunayla.shoppinglist.data.db.ShoppingDatabase
+import com.abunayla.shoppinglist.data.repositories.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this).get(ShoppingViewModel::class.java)
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+
+        val viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
+
+
     }
 }
