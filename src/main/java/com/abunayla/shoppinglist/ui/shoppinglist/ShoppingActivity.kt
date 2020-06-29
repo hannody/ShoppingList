544d9226f991f6bfb4033ca390bcfe6cdb3be240
hannody <aljaboori@gmail.com>
2020-06-29 16:41:29 +0800

    [Update] Dependency injection with Kodein.

--- src/main/java/com/abunayla/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ src/main/java/com/abunayla/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -11,16 +11,24 @@ import com.abunayla.shoppinglist.data.repositories.ShoppingRepository
 import com.abunayla.shoppinglist.others.ShoppingItemAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.lifecycle.Observer
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
+    override val kodein by kodein()
+
+    private val factory: ShoppingViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /* Replaced with kodien instance()
         val database = ShoppingDatabase(this)
         val repository = ShoppingRepository(database)
         val factory = ShoppingViewModelFactory(repository)
-
+*/
         val viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
 
         val adapter = ShoppingItemAdapter(listOf(), viewModel)
